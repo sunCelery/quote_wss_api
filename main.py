8222ae82e9b683d30abea473ae7aad069b137d6b
Dmitry Lvovich Grachev <grdmitriyl@gmail.com>
2023-04-03 20:11:18 +0300

    new: endpoint for '/{pair_name:str}'

--- main.py
+++ main.py
@@ -12,5 +12,17 @@ async def courses(websocket: WebSocket):
         data = await websocket.receive_text()
         with open('quotes.json', 'r') as f:
             quotes = json.loads(f.read())
+        await websocket.send_json(quotes)
 
-        await websocket.send_text(f'{quotes}')
+
+@app.websocket('/{pair_name:str}')
+async def courses(websocket: WebSocket, pair_name: str):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        with open('quotes.json', 'r') as f:
+            quotes = json.loads(f.read())
+        response = {
+                pair_name: f'{quotes[pair_name]}'
+            }
+        await websocket.send_json(response)
