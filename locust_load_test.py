ccfce72e1e22d2cf279831db07c106bb9c20c1e4
Dmitry Lvovich Grachev <grdmitriyl@gmail.com>
2023-04-04 18:46:59 +0300

    new: locust test 400 RPS

---
+++ locust_load_test.py
@@ -0,0 +1,57 @@
+# "http://localhost:8000"
+import time
+import json
+from locust import task
+from locust_plugins.users import SocketIOUser
+
+
+class MySocketIOUser(SocketIOUser):
+    # wait_time = 1
+    max_wait = 1
+    min_wait = 1
+    user_count = 100
+    spawn_rate = 20
+
+    def on_start(self):
+        self.connect("ws://localhost:8000/courses")
+
+    @task(1)
+    def my_task1(self):
+        self.my_value = None
+
+        # self.connect("ws://localhost:8000/courses")
+
+        # example of subscribe
+        self.send('42["subscribe",{"url":"/courses","sendInitialUpdate": true}]')
+
+        # wait until I get a push message to on_message
+        # while not self.my_value:
+        #     time.sleep(0.01)
+
+        # wait for additional pushes, while occasionally sending heartbeats, like a real client would
+        self.sleep_with_heartbeat(1)
+
+    @task(1)
+    def my_task2(self):
+        self.my_value = None
+
+        # self.connect("ws://localhost:8000/BTC-USDT")
+
+        # example of subscribe
+        self.send('42["subscribe",{"url":"/BTC-USDT","sendInitialUpdate": true}]')
+
+        # wait until I get a push message to on_message
+        # while not self.my_value:
+        #     time.sleep(0.01)
+
+        # wait for additional pushes, while occasionally sending heartbeats, like a real client would
+        self.sleep_with_heartbeat(1)
+
+    def on_message(self, message):
+        self.my_value = json.loads(message)["BTC-USDT"]
+
+
+if __name__ == "__main__":
+    host = "http://localhost:8000"
+    MySocketIOUser.protocol = "ws"
+    # MySocketIOUser.path = "/courses"
