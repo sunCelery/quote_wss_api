3b5d944654c0a1a48894740f4fcb5555c122a775
Dmitry Lvovich Grachev <grdmitriyl@gmail.com>
2023-04-06 00:32:49 +0300

    update: locust test
    	- locust file name now locustfile.py like in docs
    	- now can handle more than 1500 RPM depends of system
    	- dedicatet docker-compose file updated

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,3 @@
-# "http://localhost:8000"
 import time
 import json
 from locust import task
@@ -6,8 +5,6 @@ from locust_plugins.users import SocketIOUser
 
 
 class MySocketIOUser(SocketIOUser):
-    max_wait = 1
-    min_wait = 1
     user_count = 1500
     spawn_rate = 250
 
@@ -18,33 +15,29 @@ class MySocketIOUser(SocketIOUser):
     def my_task1(self):
         self.my_value = None
 
-        # self.connect("ws://localhost:8000/courses")
-
         # example of subscribe
         self.send('42["subscribe",{"url":"/courses","sendInitialUpdate": true}]')
 
         # wait until I get a push message to on_message
-        # while not self.my_value:
-        #     time.sleep(0.01)
+        while not self.my_value:
+            time.sleep(0.01)
 
         # wait for additional pushes, while occasionally sending heartbeats, like a real client would
-        self.sleep_with_heartbeat(1)
+        self.sleep_with_heartbeat(0.01)
 
     @task(1)
     def my_task2(self):
         self.my_value = None
 
-        # self.connect("ws://localhost:8000/BTC-USDT")
-
         # example of subscribe
         self.send('42["subscribe",{"url":"/BTC-USDT","sendInitialUpdate": true}]')
 
         # wait until I get a push message to on_message
-        # while not self.my_value:
-        #     time.sleep(0.01)
+        while not self.my_value:
+            time.sleep(0.01)
 
         # wait for additional pushes, while occasionally sending heartbeats, like a real client would
-        self.sleep_with_heartbeat(1)
+        self.sleep_with_heartbeat(0.01)
 
     def on_message(self, message):
         self.my_value = json.loads(message)["BTC-USDT"]
@@ -52,4 +45,3 @@ class MySocketIOUser(SocketIOUser):
 
 if __name__ == "__main__":
     host = "http://localhost:8000"
-    MySocketIOUser.protocol = "ws"
